fd2a70b0430f66d4ff49dbf7185b5b4985fa0a1c
Mark Sarcevicz <smark@broadinstitute.org>
2020-04-16 13:38:44 -0400

    possible working ingress config

--- dev/argocd/delete.sh
+++ dev/argocd/delete.sh
@@ -4,4 +4,5 @@ helm delete argocd-server --namespace argocd
 helm delete argocd-dex-server --namespace argocd
 helm delete argocd-repo-server --namespace argocd
 helm delete redis-sa-haproxy --namespace argocd
+helm delete argo-secrets --namespace argocd
 helm delete jade --namespace argocd
--- dev/argocd/deploy.sh
+++ dev/argocd/deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+helm namespace upgrade argocd-certificate monster/gcp-managed-cert --install --namespace argocd --version=0.1.1 -f managedcert.yaml
 helm namespace upgrade argocd-application-controller datarepo-helm/serviceaccount-psp --install --namespace argocd --set serviceAccount.name=argocd-application-controller
 helm namespace upgrade argocd-server datarepo-helm/serviceaccount-psp --install --namespace argocd --set serviceAccount.name=argocd-server
 helm namespace upgrade argocd-dex-server datarepo-helm/serviceaccount-psp --install --namespace argocd --set serviceAccount.name=argocd-dex-server
 helm namespace upgrade argocd-repo-server datarepo-helm/serviceaccount-psp --install --namespace argocd --set serviceAccount.name=argocd-repo-server
 helm namespace upgrade redis-sa-haproxy datarepo-helm/serviceaccount-psp --install --namespace argocd --set serviceAccount.name=redis-sa-haproxy
+helm namespace upgrade argo-secrets datarepo-helm/create-secret-manager-secret --version=0.0.4 --install --namespace argocd -f secrets.yaml
 helm namespace upgrade jade argo/argo-cd --install --namespace argocd -f argocd.yaml
