9f043c1a32a65804a0a3d93b328106b1f1eabdd6
Mark Sarcevicz <smark@broadinstitute.org>
2020-04-27 13:22:54 -0400

    added working whitelist for argocd and prometheus

--- integration/monitoring/prometheusOperatorInstall.sh
+++ integration/monitoring/prometheusOperatorInstall.sh
@@ -7,5 +7,6 @@ kubectl apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/re
 kubectl apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/release-0.36/example/prometheus-operator-crd/monitoring.coreos.com_servicemonitors.yaml
 kubectl apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/release-0.36/example/prometheus-operator-crd/monitoring.coreos.com_thanosrulers.yaml
 
+helm namespace upgrade ip-whitelist datarepo-helm/backend-config --install --namespace monitoring
 helm namespace upgrade monitoring-secrets datarepo-helm/create-secret-manager-secret --version=0.0.4 --install --namespace monitoring -f monitoringSecrets.yaml
 helm namespace upgrade jade-monitoring stable/prometheus-operator --version=8.10.0 --install --namespace monitoring  -f monitoring.yaml
