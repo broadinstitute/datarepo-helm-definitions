a6b5df95005fdbee2a820cf52d2c7f76e9397b2f
Mark Sarcevicz <smark@broadinstitute.org>
2020-05-20 15:58:07 -0400

    changes definitions for intgetration for datarepo chart 0.1.6

--- dev/deployMultiIntegration.sh
+++ dev/deployMultiIntegration.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 NAMESPACES=("dd" "fb" "jh" "mk" "mm" "ms" "my" "rc" "sh")
+helm repo update
 for i in "${NAMESPACES[@]}"
 do
    helm namespace upgrade ${i}-secrets datarepo-helm/create-secret-manager-secret --version=0.0.4 --install --namespace ${i} -f "${i}/${i}Secrets.yaml"
--- integration/deployMultiIntegration.sh
+++ integration/deployMultiIntegration.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 NAMESPACES=("integration-1" "integration-2" "integration-3" "integration-4" "integration-5")
-
-helm repo add monster https://broadinstitute.github.io/monster-helm
 helm repo update
 for i in "${NAMESPACES[@]}"
 do
-   helm namespace upgrade ${i}-certificate  monster/gcp-managed-cert --version=0.1.1 --install --namespace ${i} -f "${i}/${i}Managedcert.yaml"
    helm namespace upgrade ${i}-secrets datarepo-helm/create-secret-manager-secret --version=0.0.4 --install --namespace ${i} -f "${i}/${i}Secrets.yaml"
-   helm namespace upgrade ${i}-jade datarepo-helm/datarepo --version=0.1.5 --install --namespace ${i} -f "${i}/${i}Deployment.yaml"
+   helm namespace upgrade ${i}-jade datarepo-helm/datarepo --version=0.1.6 --install --namespace ${i} -f "${i}/${i}Deployment.yaml"
    sleep 5
 done
