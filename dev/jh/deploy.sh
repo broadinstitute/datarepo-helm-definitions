ada44ec424a2052634615a1d138d0cd84e340744
Mark Sarcevicz <smark@broadinstitute.org>
2020-05-20 15:47:43 -0400

    added needed values for all dev envs to move to datarepo chart 0.1.6

--- dev/jh/deploy.sh
+++ dev/jh/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 helm namespace upgrade jh-secrets datarepo-helm/create-secret-manager-secret --version=0.0.4 --install --namespace jh -f jhSecrets.yaml
-helm namespace upgrade jh-jade datarepo-helm/datarepo --version=0.0.8 --install --namespace jh -f jhDeployment.yaml
+helm namespace upgrade jh-jade datarepo-helm/datarepo --version=0.1.6 --install --namespace jh -f jhDeployment.yaml
