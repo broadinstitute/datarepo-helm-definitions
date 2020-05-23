910faa7196c6023f36616977c0eff724433adcb7
Mark Sarcevicz <smark@broadinstitute.org>
2020-05-23 09:33:09 -0400

    bumped to 0.1.7

--- dev/dd/deploy.sh
+++ dev/dd/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 helm namespace upgrade dd-secrets datarepo-helm/create-secret-manager-secret --version=0.0.4 --install --namespace dd -f ddSecrets.yaml
-helm namespace upgrade dd-jade datarepo-helm/datarepo --version=0.1.6 --install --namespace dd -f ddDeployment.yaml
+helm namespace upgrade dd-jade datarepo-helm/datarepo --version=0.1.7 --install --namespace dd -f ddDeployment.yaml
