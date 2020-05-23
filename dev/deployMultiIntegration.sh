8e8acf8dffe2a86f1c6e36447aaeee093244b960
Mark Sarcevicz <smark@broadinstitute.org>
2020-05-23 09:52:13 -0400

    made sleep smaller

--- dev/deployMultiIntegration.sh
+++ dev/deployMultiIntegration.sh
@@ -6,5 +6,5 @@ for i in "${NAMESPACES[@]}"
 do
    helm namespace upgrade ${i}-secrets datarepo-helm/create-secret-manager-secret --version=0.0.4 --install --namespace ${i} -f "${i}/${i}Secrets.yaml"
    helm namespace upgrade ${i}-jade datarepo-helm/datarepo --version=0.1.7 --install --namespace ${i} -f "${i}/${i}Deployment.yaml"
-   sleep 5
+   sleep 3
 done
