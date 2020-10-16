8790f7a413336bdfd7388024dc68afd11c8d6982
Mark Sarcevicz <smark@broadinstitute.org>
2020-10-16 15:14:28 -0400

    revert int

---
+++ integration/deployMultiIntegration.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+NAMESPACES=("integration-1" "integration-2" "integration-3" "integration-4" "integration-5")
+helm repo update
+for i in "${NAMESPACES[@]}"
+do
+   helm namespace upgrade ${i}-secrets datarepo-helm/create-secret-manager-secret --version=0.0.4 --install --namespace ${i} -f "${i}/${i}Secrets.yaml"
+   helm namespace upgrade ${i}-jade datarepo-helm/datarepo --version=0.1.9 --install --namespace ${i} -f "${i}/${i}Deployment.yaml"
+   sleep 3
+done
