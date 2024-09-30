86346f593a78baf0384ded8ab7a5695546dca3b0
Shelby Holden <sholden@broadinstitute.org>
2024-09-30 11:18:56 -0400

    Scripts appear to all be out of date and reference the umbrella style deployment (which is no longer used in the personal dev environments)

--- scripts/crds/helm-operator/installHelmOperator.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-helm repo add fluxcd https://charts.fluxcd.io
-helm repo update
-kubectl apply -f https://raw.githubusercontent.com/fluxcd/helm-operator/1.0.0/deploy/crds.yaml
-
-helm namespace upgrade -i helm-operator fluxcd/helm-operator -n fluxcd -f helm-operator.yaml
